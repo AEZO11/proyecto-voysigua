396d9285e3938dab782e93ebe5e1e1c2eedb5a56
AEZO11 <137855077+AEZO11@users.noreply.github.com>
2025-04-30 13:19:56 -0600

    Update MyComponent.js

--- src/componentes/MyComponent.js
+++ src/componentes/MyComponent.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { FaSave, FaEdit } from "react-icons/fa";
 import { MdDelete, MdCancel } from "react-icons/md";
-import "./MyComponent.css";
+import "./GestionUsuarios.css";
 import Swal from "sweetalert2";
 import Adminlte from "./adminlte";
 import { tienePermiso } from "../Utils/permisos"; // ajusta la ruta según tu estructura
@@ -17,6 +17,7 @@ const MyComponent = () => {
   const [cantidad, setCantidad] = useState(3);
   const [editingUserId, setEditingUserId] = useState(null);
   const [editedUserData, setEditedUserData] = useState({});
+  const [paginaActual, setPaginaActual] = useState(1);
 
   useEffect(() => {
     if (location.pathname === "/Gestion_Usuario") {
@@ -47,7 +48,13 @@ const MyComponent = () => {
 
   const handleEditClick = (usuario) => {
     setEditingUserId(usuario.id);
-    setEditedUserData(usuario);
+    setEditedUserData({
+      cod_persona: usuario.cod_persona,
+      nombre: usuario.nombre,
+      password: "", // dejar vacío si no se quiere cambiar
+      username: usuario.username,
+      estado: usuario.estado
+    });
   };
 
   const handleSaveClick = async () => {
@@ -146,6 +153,7 @@ const MyComponent = () => {
     usuario.nombre.toLowerCase().includes(textoFiltro.toLowerCase())
   );
 
+  const totalPaginas = Math.ceil(usuariosFiltradas.length / cantidad);
   return (
     <div>
       <Adminlte />
@@ -167,7 +175,7 @@ const MyComponent = () => {
                 >
                   <option value="3">3</option>
                   <option value="10">10</option>
-                  <option value="50">50</option>
+                  <option value="20">20</option>
                 </select>
               </label>
               <input
@@ -200,128 +208,133 @@ const MyComponent = () => {
               </thead>
               <tbody>
                 {usuariosFiltradas.length > 0 ? (
-                  usuariosFiltradas.map((usuario) => (
-                    <tr key={usuario.id}>
-                      <td>{usuario.id}</td>
-                      <td>{usuario.cod_persona}</td>
-                      <td>
-                        {editingUserId === usuario.id ? (
-                          <input
-                            className="input-editar"
-                            type="text"
-                            name="nombre"
-                            value={editedUserData.nombre || ""}
-                            onChange={(e) =>
-                              setEditedUserData({
-                                ...editedUserData,
-                                nombre: e.target.value,
-                              })
-                            }
-                          />
-                        ) : (
-                          usuario.nombre
-                        )}
-                      </td>
-                      <td>
-                        {editingUserId === usuario.id ? (
-                          <input
-                            className="input-editar"
-                            type="text"
-                            name="password"
-                            value={editedUserData.password || ""}
-                            onChange={(e) =>
-                              setEditedUserData({
-                                ...editedUserData,
-                                password: e.target.value,
-                              })
-                            }
-                          />
-                        ) : (
-                          "*******"
-                        )}
-                      </td>
-                      <td> {usuario.remember_token}</td>
-                      <td>
-                        {editingUserId === usuario.id ? (
-                          <input
-                            className="input-editar"
-                            type="text"
-                            name="username"
-                            value={editedUserData.username || ""}
-                            onChange={(e) =>
-                              setEditedUserData({
-                                ...editedUserData,
-                                username: e.target.value,
-                              })
-                            }
-                          />
-                        ) : (
-                          usuario.username
-                        )}
-                      </td>
-                      <td>
-                        {editingUserId === usuario.id ? (
-                          <input
-                            className="input-editar"
-                            type="number"
-                            name="estado"
-                            value={editedUserData.estado || ""}
-                            onChange={(e) =>
-                              setEditedUserData({
-                                ...editedUserData,
-                                estado: e.target.value,
-                              })
-                            }
-                          />
-                        ) : (
-                          usuario.estado
-                        )}
-                      </td>
-                      <td> {usuario.primera_vez} </td>
-                      <td> {usuario.fecha_vencimiento} </td>
-                      <td>
-                        {editingUserId === usuario.id ? (
-                          <>
-                            {tienePermiso("actualizar_usuarios") && (
-                              <button
-                                onClick={handleSaveClick}
-                                className="new-user-save-btn"
-                              >
-                                <FaSave /> Guardar{" "}
-                              </button>
-                            )}
-                            {tienePermiso("cancelar_usuarios") && (
-                              <button
-                                className="new-user-cancel-btn"
-                                onClick={() => setEditingUserId(null)}
-                              >
-                                <MdCancel /> Cancelar
-                              </button>
-                            )}
-                          </>
-                        ) : (
-                          <>
-                            {tienePermiso("editar_usuarios") && (
-                              <button
-                                className="new-user-edit-btn"
-                                onClick={() => handleEditClick(usuario)}
-                              >
-                                <FaEdit /> Editar
-                              </button>
-                            )}
-                            {tienePermiso("eliminar_usuarios") && (
-                              <button
-                                className="new-user-delete-btn"
-                                onClick={() => handleDelete(usuario.id)}
-                              >
-                                <MdDelete /> Eliminar
-                              </button>
-                            )}
-                          </>
-                        )}
-                      </td>
-                    </tr>
-                  ))
+                  usuariosFiltradas
+                    .slice(
+                      (paginaActual - 1) * cantidad,
+                      paginaActual * cantidad
+                    )
+                    .map((usuario) => (
+                      <tr key={usuario.id}>
+                        <td>{usuario.id}</td>
+                        <td>{usuario.cod_persona}</td>
+                        <td>
+                          {editingUserId === usuario.id ? (
+                            <input
+                              className="input-editar"
+                              type="text"
+                              name="nombre"
+                              value={editedUserData.nombre || ""}
+                              onChange={(e) =>
+                                setEditedUserData({
+                                  ...editedUserData,
+                                  nombre: e.target.value,
+                                })
+                              }
+                            />
+                          ) : (
+                            usuario.nombre
+                          )}
+                        </td>
+                        <td>
+                          {editingUserId === usuario.id ? (
+                            <input
+                              className="input-editar"
+                              type="text"
+                              name="password"
+                              value={editedUserData.password || ""}
+                              onChange={(e) =>
+                                setEditedUserData({
+                                  ...editedUserData,
+                                  password: e.target.value,
+                                })
+                              }
+                            />
+                          ) : (
+                            "*******"
+                          )}
+                        </td>
+                        <td> {usuario.remember_token}</td>
+                        <td>
+                          {editingUserId === usuario.id ? (
+                            <input
+                              className="input-editar"
+                              type="text"
+                              name="username"
+                              value={editedUserData.username || ""}
+                              onChange={(e) =>
+                                setEditedUserData({
+                                  ...editedUserData,
+                                  username: e.target.value,
+                                })
+                              }
+                            />
+                          ) : (
+                            usuario.username
+                          )}
+                        </td>
+                        <td>
+                          {editingUserId === usuario.id ? (
+                            <input
+                              className="input-editar"
+                              type="number"
+                              name="estado"
+                              value={editedUserData.estado || ""}
+                              onChange={(e) =>
+                                setEditedUserData({
+                                  ...editedUserData,
+                                  estado: e.target.value,
+                                })
+                              }
+                            />
+                          ) : (
+                            usuario.estado
+                          )}
+                        </td>
+                        <td> {usuario.primera_vez} </td>
+                        <td> {usuario.fecha_vencimiento} </td>
+                        <td>
+                          {editingUserId === usuario.id ? (
+                            <>
+                              {tienePermiso("actualizar_usuarios") && (
+                                <button
+                                  onClick={handleSaveClick}
+                                  className="new-user-save-btn"
+                                >
+                                  <FaSave /> Guardar{" "}
+                                </button>
+                              )}
+                              {tienePermiso("cancelar_usuarios") && (
+                                <button
+                                  className="new-user-cancel-btn"
+                                  onClick={() => setEditingUserId(null)}
+                                >
+                                  <MdCancel /> Cancelar
+                                </button>
+                              )}
+                            </>
+                          ) : (
+                            <>
+                              {tienePermiso("editar_usuarios") && (
+                                <button
+                                  className="new-user-edit-btn"
+                                  onClick={() => handleEditClick(usuario)}
+                                >
+                                  <FaEdit /> Editar
+                                </button>
+                              )}
+                              {tienePermiso("eliminar_usuarios") && (
+                                <button
+                                  className="new-user-delete-btn"
+                                  onClick={() => handleDelete(usuario.id)}
+                                >
+                                  <MdDelete /> Eliminar
+                                </button>
+                              )}
+                            </>
+                          )}
+                        </td>
+                      </tr>
+                    ))
                 ) : (
                   <tr>
                     <td colSpan="13">No hay usuarios disponibles.</td>
@@ -329,6 +342,35 @@ const MyComponent = () => {
                 )}
               </tbody>
             </table>
+            <div className="paginacion">
+              <button
+                onClick={() => setPaginaActual(1)}
+                disabled={paginaActual === 1}
+              >
+                {"<<"}
+              </button>
+              <button
+                onClick={() => setPaginaActual(paginaActual - 1)}
+                disabled={paginaActual === 1}
+              >
+                {"<"}
+              </button>
+              <span>
+                Página {paginaActual} de {totalPaginas}
+              </span>
+              <button
+                onClick={() => setPaginaActual(paginaActual + 1)}
+                disabled={paginaActual === totalPaginas}
+              >
+                {">"}
+              </button>
+              <button
+                onClick={() => setPaginaActual(totalPaginas)}
+                disabled={paginaActual === totalPaginas}
+              >
+                {">>"}
+              </button>
+            </div>
           </div>
         </section>
       </div>
